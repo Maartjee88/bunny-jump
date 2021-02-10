ca5cca6f5e40407636856acb575aed10b0618efb
Maartje <amvankranen@gmail.com>
2021-02-10 20:01:25 +0100

    feat(gamefield): added wraparound

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -59,6 +59,9 @@ export default class Game extends Phaser.Scene {
 
         // follow player
         this.cameras.main.startFollow(this.player)
+
+        // set deadzone
+        this.cameras.main.setDeadzone(this.scale.width * 1.5)
     }
 
     update() { // CAUTION: performs every frame
@@ -88,6 +91,20 @@ export default class Game extends Phaser.Scene {
         }
         else {
             this.player.setVelocityX(0)
+        }
+
+        this.horizontalWrap(this.player)
+    }
+
+    /** @param {Phaser.GameObjects.Sprite} sprite */
+    horizontalWrap(sprite) {
+        const halfWidth = sprite.displayWidth * 0.5
+        const gameWidth = this.scale.width
+
+        if (sprite.x < -halfWidth) {
+            sprite.x = gameWidth + halfWidth
+        } else if (sprite.x > gameWidth + halfWidth) {
+            sprite.x = -halfWidth
         }
     }
 }
